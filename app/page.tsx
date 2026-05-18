d7943665c67ae9455e62945331406544d7202764
Irasov <srnan@yandex.ru>
2026-05-18 11:49:56 +0500

    feat: show products

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,21 @@ import {
   TopBar,
   Filtres,
   ProductsGroupList,
-} from "@/components/shared"
+} from "@/components/shared";
+import {prisma} from '../prisma/prisma-client';
 
-export default function Page() {
+export default async function Page() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingridient: true,
+          items: true,
+        }
+      }
+    }
+  })
+
   return (
     <>
       <Container className="mt-10">
@@ -23,75 +35,18 @@ export default function Page() {
           {/* Список товаров */}
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroupList
-                title="Пиццы"
-                items={[
-                  {
-                    id: 1,
-                    name: "Пепперони",
-                    imageUrl: "https://i.postimg.cc/p5JgFRPm/pizza-1.png",
-                    price: 1000,
-                    items: [{ price: 1000 }],
-                  },
-                  {
-                    id: 2,
-                    name: "Пепперони",
-                    imageUrl: "https://i.postimg.cc/p5JgFRPm/pizza-1.png",
-                    price: 1000,
-                    items: [{ price: 1000 }],
-                  },
-                  {
-                    id: 3,
-                    name: "Пепперони",
-                    imageUrl: "https://i.postimg.cc/p5JgFRPm/pizza-1.png",
-                    price: 1000,
-                    items: [{ price: 1000 }],
-                  },
-                  {
-                    id: 4,
-                    name: "Пепперони",
-                    imageUrl: "https://i.postimg.cc/p5JgFRPm/pizza-1.png",
-                    price: 1000,
-                    items: [{ price: 1000 }],
-                  },
-                ]}
-                categoryId={1}
-              />
-
-              <ProductsGroupList
-                title="Комбо"
-                categoryId={2}
-                items={[
-                  {
-                    id: 6,
-                    name: "Пепперони",
-                    imageUrl: "https://i.postimg.cc/p5JgFRPm/pizza-1.png",
-                    price: 1000,
-                    items: [{ price: 1000 }],
-                  },
-                  {
-                    id: 7,
-                    name: "Пепперони",
-                    imageUrl: "https://i.postimg.cc/p5JgFRPm/pizza-1.png",
-                    price: 1000,
-                    items: [{ price: 1000 }],
-                  },
-                  {
-                    id: 8,
-                    name: "Пепперони",
-                    imageUrl: "https://i.postimg.cc/p5JgFRPm/pizza-1.png",
-                    price: 1000,
-                    items: [{ price: 1000 }],
-                  },
-                  {
-                    id: 9,
-                    name: "Пепперони",
-                    imageUrl: "https://i.postimg.cc/p5JgFRPm/pizza-1.png",
-                    price: 1000,
-                    items: [{ price: 1000 }],
-                  },
-                ]}
-              />
+              {
+                categories.map((category) => 
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      categoryId={category.id}
+                      items={category.products} 
+                    />
+                  )
+                )  
+              }
             </div>
           </div>
         </div>
