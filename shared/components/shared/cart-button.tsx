c2827af8935fa3e66275094678fea5cec806b571
Irasov <srnan@yandex.ru>
2026-06-02 11:18:32 +0500

    feat: add loading and totalAmount and count items for CartButton

--- shared/components/shared/cart-button.tsx
+++ shared/components/shared/cart-button.tsx
@@ -1,22 +1,27 @@
-import { cn } from "@/shared/lib/utils"
-import React from "react"
-import { Button } from "../ui"
-import { ArrowRight, ShoppingCart } from "lucide-react"
-import { CartDrawer } from "./cart-drawer"
+'use client';
 
+import { cn } from "@/shared/lib/utils";
+import React from "react";
+import { Button } from "../ui";
+import { ArrowRight, ShoppingCart } from "lucide-react";
+import { CartDrawer } from "./cart-drawer";
+import { useCartStore } from "@/shared/store";
 interface Props {
   className?: string
 }
 
 export const CartButton: React.FC<Props> = ({ className }) => {
+  const totalAmount = useCartStore((state) => state.totalAmount);
+  const loading = useCartStore((state) => state.loading);
+  const items = useCartStore((state) => state.items);
   return (
     < CartDrawer>
-      <Button className={cn("group relative", className)}>
-        <b>520 ₽</b>
+      <Button loading={loading} className={cn("group relative", {'w-[105px]': loading} ,className)}>
+        <b>{totalAmount} ₽</b>
         <span className="mx-3 h-full w-[1px] bg-white/30" />
         <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
           <ShoppingCart size={16} className="relative" strokeWidth={2} />
-          <b>3</b>
+          <b>{items.length}</b>
         </div>
         <ArrowRight
           size={20}
