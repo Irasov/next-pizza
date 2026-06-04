cf8f6a09b2c1833f84a024ac072d704a0cda0526
Irasov <srnan@yandex.ru>
2026-06-04 10:20:00 +0500

    feat: add empty cart

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -2,7 +2,9 @@
 
 import React from "react"
 import { Button } from "../ui";
-import { ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { Title } from "./";
+import { cn } from "@/shared/lib/utils";
 
 
 import {
@@ -20,6 +22,7 @@ import { CartDrawerItem } from "./";
 import { getCartItemDetails } from "@/shared/lib";
 import { useCartStore } from "@/shared/store";
 import { PizzaType, PizzaSize } from "@/shared/constants/pizza";
+import Image from "next/image";
 
 interface Props {
   className?: string
@@ -46,57 +49,82 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({children, 
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
       <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
-        <SheetHeader>
-          <SheetTitle>
-            В корзине <span className="font-bold">{items.length} товара</span>
-          </SheetTitle>
-        </SheetHeader>
-        <div className="mt-5 overflow-auto flex-1">
-          {
-            items.map((item)=> (
-              <div className="mb-2" key={item.id}>
-                <CartDrawerItem 
-                  id={item.id}
-                  imageUrl={item.imageUrl}
-                  details={
-                    item.pizzaSize && item.pizzaType 
-                    ? getCartItemDetails(
-                      item.ingredients, 
-                      item.pizzaType as PizzaType, 
-                      item.pizzaSize as PizzaSize,
-                    )
-                    : ''
-                  }
-                  disabled={item.disabled}
-                  name={item.name}
-                  price={item.price}
-                  quantity={item.quantity}
-                  onClickCountButton = {(type) => onClickCountButton(item.id, item.quantity, type)}
-                  onClickRemove={() => removeCartItem(item.id)}
-                />
-              </div>
-            ))
-          }
-        </div>
-        <SheetFooter className="bg-white p-8">
-          <div className="w-full">
-            <div className="flex mb-4">
-              <span className="flex flex-1 text-lg text-neutral-500">
-                Итого
-                <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
-              </span>
-              <span className="font-bold text-lg">{totalAmount}</span>
+        <div className={cn('flex flex-col h-full', !totalAmount && 'justify-center')}>
+          { totalAmount > 0 && (
+            <SheetHeader>
+              <SheetTitle>
+                В корзине <span className="font-bold">{items.length} товара</span>
+              </SheetTitle>
+            </SheetHeader>
+          )}
+
+          {!totalAmount && (
+            <div className="flex flex-col items-center justify-center w-72 mx-auto">
+              <Image src="/assets/images/empty-box.png" alt="Empty cart" width={120} height={120} />
+              <Title size="sm" text="В корзине нет товаров" className="text-center foont-bold my-2" />
+              <p className="text-center text-neutral-500 mb-5">
+                Добавьте хотя бы оду пиццу, чтобы оформить заказ
+              </p>
+              <SheetClose>
+                <Button className="w-56 h-12 text-base" size="lg">
+                  <ArrowLeft className="w-5 mr-2" />
+                    Вернуться к покупкам
+                </Button>
+              </SheetClose>
             </div>
-            <Link href="/cart">
-              <Button 
-                type="submit"
-                className="w-full h-12 text-base">
-                  Оформить заказ
-                  <ArrowRight className="w-5 ml-2" />
-              </Button>
-            </Link>
-          </div>
-        </SheetFooter>
+          )}
+
+          { totalAmount > 0 && (
+            <>
+              <div className="mt-5 overflow-auto flex-1">
+                {
+                  items.map((item)=> (
+                    <div className="mb-2" key={item.id}>
+                      <CartDrawerItem 
+                        id={item.id}
+                        imageUrl={item.imageUrl}
+                        details={
+                          item.pizzaSize && item.pizzaType 
+                          ? getCartItemDetails(
+                            item.ingredients, 
+                            item.pizzaType as PizzaType, 
+                            item.pizzaSize as PizzaSize,
+                          )
+                          : ''
+                        }
+                        disabled={item.disabled}
+                        name={item.name}
+                        price={item.price}
+                        quantity={item.quantity}
+                        onClickCountButton = {(type) => onClickCountButton(item.id, item.quantity, type)}
+                        onClickRemove={() => removeCartItem(item.id)}
+                      />
+                    </div>
+                  ))
+                }
+              </div>
+              <SheetFooter className="bg-white p-8">
+                <div className="w-full">
+                  <div className="flex mb-4">
+                    <span className="flex flex-1 text-lg text-neutral-500">
+                      Итого
+                      <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
+                    </span>
+                    <span className="font-bold text-lg">{totalAmount}</span>
+                  </div>
+                  <Link href="/cart">
+                    <Button 
+                      type="submit"
+                      className="w-full h-12 text-base">
+                        Оформить заказ
+                        <ArrowRight className="w-5 ml-2" />
+                    </Button>
+                  </Link>
+                </div>
+              </SheetFooter>
+            </>
+          )}
+        </div>
       </SheetContent>
     </Sheet>
   )
