4dd04a42380cd1e144f1abcc40122d589905190a
Irasov <srnan@yandex.ru>
2026-06-16 17:29:29 +0500

    feat: forms authorize

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,78 @@
 import NextAuth from "next-auth";
 import GitHubProvider from 'next-auth/providers/github';
+import { CredentialsProvider } from "next-auth/providers/credentials";
+import { email } from "zod";
+import prisma from "@/prisma/prisma-client";
+import { compare } from "bcrypt";
+import { Session } from "node:inspector";
 
 export const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
-    })
-  ]
+    }),
+    // CredentialsProvider({
+    //   name: 'Credentials',
+    //   credentials: {
+    //     email: {label: 'Email', type: 'text'},
+    //     password: {label: 'Paasword', type: 'password'}
+    //   },
+    //   async authorize(credentials) {
+    //     if(!credentials) {
+    //       return null
+    //     }
+    //     const values = {
+    //       email: credentials.email,
+    //     };
+
+    //     const findUser = await prisma.user.findFirst({
+    //       where: values,
+    //     });
+
+    //     if (!findUser) {
+    //       return null
+    //     }
+
+    //     const isPasswordValid = await compare(credentials.password, findUser.password)
+
+    //     if(!isPasswordValid) {
+    //       return null
+    //     }
+
+    //     if(!findUser.verified) {
+    //       return null
+    //     }
+
+    //     return {
+    //       id: String(findUser.id),
+    //       email: findUser.email,
+    //       name: findUser.fullName,
+    //       role: findUser.role,
+    //     }
+    //   }
+    // })
+  ],
+  // secret: process.env.NEXTAUTH_SECRET,
+  // session: {
+  //   strategy: 'jwt',
+  // },
+  // callbacks: {
+  //   async jwt({token}) {
+  //     const findUser = await prisma.user.findFirst({
+  //       where: {
+  //         email: token.email,
+  //       }
+  //     });
+  //     if (findUser) {
+  //         token.id = String(findUser.id);
+  //         token.email = findUser.email;
+  //         token.token.name = findUser.fullName;
+  //         token.role = findUser.role;
+  //     }
+  //     return token;
+  //   }
+  // }
 }
 
 const handler = NextAuth(authOptions);
