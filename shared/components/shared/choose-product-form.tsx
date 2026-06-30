d807bbad0c2d2f9d49e5c06dd4dda2b16c3b6455
Игорь Ирасов <irasov.iv@shc.local>
2026-06-30 12:12:28 +0500

    refactor: change ChooseProductForm image position

--- shared/components/shared/choose-product-form.tsx
+++ shared/components/shared/choose-product-form.tsx
@@ -23,11 +23,11 @@ export const ChooseProductForm: React.FC<Props> = ({ className, name, loading, i
 
   return (
     <div className={cn("flex flex-1 max-xl:flex-col", className)}>
-      <div className="flex items-center justify-center flex-1 relative w-full ">
+      <div className="flex items-center justify-center flex-1 relative w-full m-0 p-0">
         <img 
           src={imageUrl} 
           alt={name} 
-          className="relative left-2 top-2 transition-all z-10 duration-300 w-75 h-75 max-599:w-55 max-599:h-55"
+          className="relative transition-all z-10 duration-300 w-75 h-75 max-599:w-55 max-599:h-55"
         />
       </div>
       <div className="min-w-122.5 bg-[#F7F6F6] p-7 flex flex-col justify-center items-center max-599:min-w-35 max-599:p-2">
